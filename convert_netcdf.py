a36606619e41a31d0fcfa53fddf1dec71371b4a9
agawalenkiewicz <39482333+agawalenkiewicz@users.noreply.github.com>
2019-04-01 12:10:33 +0100

    cleaned code

---
+++ convert_netcdf.py
@@ -0,0 +1,81 @@
+import netCDF4 as nc
+import time
+import numpy as np
+
+def create_netcdf(output_name, regridded_data_3d, regridded_lat_array, regridded_lon_array):
+	#open a new netcdf file in the write ('w') mode
+	dataset = nc.Dataset(output_name+'.nc', 'w', format='NETCDF4_CLASSIC')
+
+	#create dimensions
+	#band = dataset.createDimension('band', bands_len) #11 can later be not hard coded but a variable e.g. band_number
+	lat = dataset.createDimension('lat', len(regridded_lat_array[:,0])) #400 not hard coded but e.g. len(lat_array) of the regridded dataset
+	lon = dataset.createDimension('lon', len(regridded_lon_array[0,:])) #400 not hard coded but e.g. len(lon_array) of the regridded dataset
+	time = dataset.createDimension('time', None) #unlimited time, if we want to add data later
+
+	# Create coordinate variables for 4-dimensions
+	time = dataset.createVariable('time', np.float32, ('time',))
+	time.standard_name = 'time'
+	#bands = dataset.createVariable('bands', np.int32, ('band',))
+	latitudes = dataset.createVariable('latitude', np.float32, ('lat','lon',))
+	latitudes.standard_name = 'latitude'
+	longitudes = dataset.createVariable('longitude', np.float32, ('lat','lon',))
+	longitudes.standard_name = 'longitude'
+
+	# Create the actual 4-d variable
+	BT10 = dataset.createVariable('BT_band10', np.float32, ('lat','lon'))
+	BT11 = dataset.createVariable('BT_band11', np.float32, ('lat','lon'))
+	reflectance1 = dataset.createVariable('reflectance_band1', np.float32, ('lat','lon'))
+	reflectance2 = dataset.createVariable('reflectance_band2', np.float32, ('lat','lon'))
+	reflectance3 = dataset.createVariable('reflectance_band3', np.float32, ('lat','lon'))
+	reflectance4 = dataset.createVariable('reflectance_band4', np.float32, ('lat','lon'))
+	reflectance5 = dataset.createVariable('reflectance_band5', np.float32, ('lat','lon'))
+	reflectance6 = dataset.createVariable('reflectance_band6', np.float32, ('lat','lon'))
+	reflectance7 = dataset.createVariable('reflectance_band7', np.float32, ('lat','lon'))
+	#reflectance8 = dataset.createVariable('reflectance_band8', np.float32, ('lat','lon'))
+	qualityband9 = dataset.createVariable('quality_band9', np.float32, ('lat','lon'))
+	#radiance = dataset.createVariable('radiance', np.float32, ('time', 'band','lat','lon'))
+
+	#Create Global Attributes
+	#dataset.description = 'Landsat 8 regridded data'
+	#dataset.history = 'Created ' + time.ctime(time.time())
+
+	# Variable Attributes
+	latitudes.units = 'degree_north'
+	longitudes.units = 'degree_east'
+	#bands.units = 'micro_meters'
+	BT10.units = 'K'
+	BT11.units = 'K'
+	reflectance1.units = 'Watts_meter^-2'
+	reflectance2.units = 'Watts_meter^-2'
+	reflectance3.units = 'Watts_meter^-2'
+	reflectance4.units = 'Watts_meter^-2'
+	reflectance5.units = 'Watts_meter^-2'
+	reflectance6.units = 'Watts_meter^-2'
+	reflectance7.units = 'Watts_meter^-2'
+	#reflectance8.units = 'Watts_meter^-2'
+	qualityband9.units = 'Unitless'
+	#radiance.units = 'Watts_meter-2'
+	
+	#Assign values to the variables
+	lats = regridded_lat_array #np.flipud(regridded_data_3d[0,:,:]) #np.arange(-90,91,2.5)
+	lons = regridded_lon_array #regridded_data_3d[1,:,:]  #np.arange(-180,180,2.5)
+	latitudes[:,:] = lats[:,:]
+	longitudes[:,:] = lons[:,:]
+	print(latitudes)
+	print(longitudes)
+	reflectance1[:,:] = regridded_data_3d[0,:,:] #regridded_data_3d[2,:,:]
+	reflectance2[:,:] = regridded_data_3d[1,:,:] #regridded_data_3d[3,:,:]
+	reflectance3[:,:] = regridded_data_3d[2,:,:] #regridded_data_3d[4,:,:]
+	reflectance4[:,:] = regridded_data_3d[3,:,:] #regridded_data_3d[5,:,:]
+	reflectance5[:,:] = regridded_data_3d[4,:,:] #regridded_data_3d[6,:,:]
+	reflectance6[:,:] = regridded_data_3d[5,:,:] #regridded_data_3d[7,:,:]
+	reflectance7[:,:] = regridded_data_3d[6,:,:] #regridded_data_3d[8,:,:]
+	#reflectance8[:,:] = regridded_data_3d[7,:,:]
+	qualityband9[:,:] = regridded_data_3d[7,:,:] #regridded_data_3d[9,:,:]
+	BT10[:,:] = regridded_data_3d[8,:,:] #regridded_data_3d[10,:,:]
+	BT11[:,:] = regridded_data_3d[9,:,:] #regridded_data_3d[11,:,:]
+	print(BT10)
+	#print(BT11.shape)
+	
+	dataset.close()
+	return
---
+++ proj_grid.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+#import pyresample.geometry
+
+def creategrid(center_lat, center_lon, dist_deg, spacing, mesh=False):
+    """
+    Output grid within geobounds and specifised cell size.
+    Cell_size_deg should be in decimal degrees.
+    Return: common grid with center as a specific loaction
+    """
+    #calculate the max and min  values of lats from the central point
+    max_lat = center_lat + dist_deg
+    min_lat = center_lat - dist_deg
+    print("max lat" , max_lat)
+    print("min lat" , min_lat)
+    #calculate the max and min  values of lats from the central point
+    max_lon = center_lon + dist_deg
+    min_lon = center_lon - dist_deg #- 0.0000000000003
+    print("max lon" , max_lon)
+    print("min lon" , min_lon)
+    #calculate the size of the common grid (a square M by M pixels)  
+    cell_size_deg = 2 * dist_deg
+    
+    N_lat = (max_lat - min_lat) / spacing
+    N_lon = (max_lon - min_lon) / spacing
+    print("N_lon" , N_lon , "N_lat" , N_lat)
+    N_lat = int(N_lat)
+    print(N_lat)
+    N_lon = int(N_lon) #math.ceil(N_lon) #int(N_lon)
+    print(N_lon)
+    
+    lon = np.zeros([1,N_lon+1])
+    lat = np.zeros([N_lat+1,1])
+    lon_val = np.arange(min_lon, max_lon, spacing)
+    lat_val = np.arange(min_lat, max_lat, spacing)
+    lon[0,:] = lon_val
+    lat[:,0] = lat_val
+    print(lat_val.shape)
+    print(lon_val.shape)
+    
+    lon_out = lon
+    lat_out = lat
+    #lat_out = np.flipud(lat_out)
+    #np.meshgrid
+    for i in np.arange(0,N_lat,1):
+        lat_out = np.concatenate([lat_out,lat],axis=1)
+    for j in np.arange(0,N_lon,1):
+        lon_out = np.concatenate([lon_out,lon],axis=0)
+            
+
+    lat_out = np.flipud(lat_out)  
+    common_grid = np.concatenate([[lat_out], [lon_out]])
+    
+    return common_grid, min_lon, min_lat, spacing
+
+
