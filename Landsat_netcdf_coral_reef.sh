190c383044d91b0c31aff76d715ebf4ae3ece8fa
agawalenkiewicz <39482333+agawalenkiewicz@users.noreply.github.com>
2020-05-04 09:52:34 +0100

    a few comments on the .sh file

--- Landsat_netcdf_coral_reef.sh
+++ Landsat_netcdf_coral_reef.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 module load python3.5/canopy-2.1.3
+#in path specify the path where your Landsat files are
 path=/glusterfs/surft/users/mp877190/data/coral_reef/* 
 for f in $path  
 do echo This is the folder $f
@@ -7,6 +8,8 @@ do echo This is the folder $f
 meta=`find *_MTL.txt` && echo $meta 
 bands=${meta:0:40}'_B{}.TIF' && echo $bands  
 output=${meta:0:40} && echo $output
-python /home/users/mp877190/getting_netcdf/Landsat_code/main_coral_reef.py "$f/scenes" $bands $meta $output
+#change code path to where your code is stored (whole path)
+code_path=/home/users/mp877190/getting_netcdf/Landsat_code/main_coral_reef.py
+python $code_path "$f/scenes" $bands $meta $output
 cd ..
 done
